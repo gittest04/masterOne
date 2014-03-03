87ddc409199aab96cddd581983949ec6529b4765
srigars <srigar.personal@gmail.com>
2014-03-03 16:47:42 +0530

    First Commit

---
+++ src/masterOne/Sample.java
@@ -0,0 +1,21 @@
+/**
+ * Training Assignment OFS
+ */
+package masterOne;
+
+/**
+ * @author Srigar S, Trainee Software Engineer
+ *
+ * Mar 3, 2014
+ */
+public class Sample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sri");
+	}
+
+}
